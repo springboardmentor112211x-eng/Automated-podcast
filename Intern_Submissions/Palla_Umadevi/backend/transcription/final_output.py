7e064c7719fccfd6d70b59609b6c9837964bd1b6
Palla-Umadevi <pallau809@gmail.com>
2026-01-23 07:34:00 +0530

    Removed incorrect backend and frontend files

--- Intern_Submissions/Palla_Umadevi/backend/transcription/final_output.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def save_final(topics, path="final_output.json"):
-    with open(path, "w") as f:
-        json.dump({
-            "total_topics": len(topics),
-            "topics": topics
-        }, f, indent=2)
--- Intern_Submissions/Palla_Umadevi/backend/transcription/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from transcription.views import transcribe_podcast
-
-urlpatterns = [
-    path("api/transcribe/", transcribe_podcast),
-]
